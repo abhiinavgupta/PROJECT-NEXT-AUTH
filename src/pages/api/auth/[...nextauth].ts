ff295e66f269a27169f385e7142b960f40fec502
abhiinavgupta <121946359+abhiinavgupta@users.noreply.github.com>
2024-02-24 13:09:15 +0530

    providers added

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,8 @@ import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import TwitterProvider from "next-auth/providers/twitter";
 
 export default NextAuth({
   providers: [
@@ -20,6 +22,14 @@ export default NextAuth({
     GitHubProvider({
         clientId: process.env.GITHUB_ID as string,
         clientSecret: process.env.GITHUB_SECRET as string
+      }),
+      DiscordProvider({
+        clientId: process.env.DISCORD_CLIENT_ID as string,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET as string
+      }),
+      TwitterProvider({
+        clientId: process.env.TWITTER_CLIENT_ID as string,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET as string
       })
     // Passwordless / email sign in
     // EmailProvider({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { NextPageContext } from "next";
+import { useSession, signIn, signOut, getSession } from "next-auth/react"
 
 export default function Home() {
   const { data: session } = useSession()
-  console.log(session);
+  // console.log(session);
   return (
     <>
     <h1 className="text-red-500">welcome</h1>
+    <img
+                src={session?.user?.image!}
+                alt={`${session?.user?.name} image`}
+                className="rounded-full h-40 w-40"
+              />
+
+              {session ? (<button onClick={() => signOut()}>Sign out</button> ) : (<button onClick={() => signIn()}>Sign in</button>)}
+    
+    
     </>
   );
+}
+export async function getServerSideProps(ctx: NextPageContext) {
+  const session = await getSession(ctx);
+  return {
+    props: {
+      session,
+    },
+  };
 }
